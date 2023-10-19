97234feddddf0dbfc93043ca93e88cadc2e3b690
Pascal Brandt <psbrandt@gmail.com>
2023-10-19 12:38:51 -0400

    feat(jemalloc): 🗃️ Try jemallocator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() {
     deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
 }
